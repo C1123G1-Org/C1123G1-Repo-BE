0de99b0fba69da0394e2adf4e71824232ffa5954
Ngo Hoang <ngohoang8320@gmail.com>
2024-05-28 14:57:43 +0700

    Build project BE

---
+++ src/main/java/com/codegym/C1123G1RepoBeApplication.java
@@ -0,0 +1,14 @@
+package com.codegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C1123G1RepoBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C1123G1RepoBeApplication.class,
+                args);
+    }
+
+}
