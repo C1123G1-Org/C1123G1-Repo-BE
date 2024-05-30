d08d9028d72c83ed8093b24127cce5aadcd950d7
Nguyễn Minh Phương <nguyenminhphuong813@gmail.com>
2024-05-30 10:55:09 +0700

    phân trang 1 pgae + thêm + sửa + xoá exporteCote

--- src/main/java/com/codegym/dto/ExportCoteRequest.java
+++ src/main/java/com/codegym/dto/ExportCoteRequest.java
@@ -1,5 +1,8 @@
 package com.codegym.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +15,21 @@ import java.time.LocalDateTime;
 public class ExportCoteRequest {
     private int id;
     private String partner;
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private double weight;
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private int amount;
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private double price;
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private int cote_id;
     private int account_id;
     private String dateExport;
