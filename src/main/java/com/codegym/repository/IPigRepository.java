3e94a53ce3d7d985659c70d120bc97e98d6c5dba
hoangle71200 <108779064+hoangle71200@users.noreply.github.com>
2024-06-03 08:23:39 +0700

    0603

--- src/main/java/com/codegym/repository/IPigRepository.java
+++ src/main/java/com/codegym/repository/IPigRepository.java
@@ -2,6 +2,8 @@ package com.codegym.repository;
 
 import com.codegym.model.Cote;
 import com.codegym.model.Pig;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -9,12 +11,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface IPigRepository extends JpaRepository<Pig, Integer> {
-    Optional<List<Pig>> findPigsByStatus(String status);
-    Optional<List<Pig>> findPigsByStatusContaining(String status);
-    List<Pig> findPigsByWeight(double weight);
-    Optional<List<Pig>> findPigsByWeightIsAfterAndWeightIsBefore (float weightMin, float weightMax);
-    Optional<List<Pig>> findPigsByWeightIsAfter(float weightMin);
-    Optional<List<Pig>> findPigsByWeightIsBefore (float weightMax);
-    Optional<List<Pig>> findPigsByRoom(Cote room);
-    Optional<List<Pig>> findPigsByWeightBetween(double weightMin, double weightMax);
+    Page<Pig> findPigsByStatus(Pageable pageable, String status);
+//    Optional<List<Pig>> findPigsByStatusContaining(String status);
+//    List<Pig> findPigsByWeight(double weight);
+//    Optional<List<Pig>> findPigsByRoom(Cote room);
+//    Optional<List<Pig>> findPigsByWeightBetween(double weightMin, double weightMax);
 }
