2a971aa74cbe7cd666760fc80a3fd6b256771b8f
Ngo Hoang <ngohoang8320@gmail.com>
2024-06-06 15:18:07 +0700

    Fix secure

--- src/main/java/com/codegym/repository/IAccountRepository.java
+++ src/main/java/com/codegym/repository/IAccountRepository.java
@@ -7,14 +7,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface IAccountRepository extends JpaRepository<Account, Integer> {
 
-    @Query(value = " select * from account where username like :name", nativeQuery = true)
-    Page<Account> findAllPage(Pageable pageable,@Param("name") String name);
+    @Query(value = " select * from account where username like :name",
+            nativeQuery = true)
+    Page<Account> findAllPage(Pageable pageable,
+                              @Param("name") String name);
 
 
-
+    Account findByUsername(String username);
 
 }
